827b79615e1de9d55b3755eae6c5c1e13b3e4197
Oleksii Lisikh <oleksii.lisikh@gmail.com>
2026-05-26 22:49:19 +0200

    fix: No tests are found when no build.gradle[.kts] file present in root (#1)
    When there's no build.gradle[.kts] file present in a root folder of the
    project (which is the case if `gradle init` command is used),
    neotest-gradle can't run tests in this case.
    
    Another issue is that if an empty build.gradle[.kts] file is added to
    the root, neotest-gradle still can't run tests because the filter
    command turns out to be invalid.
    
    This PR is fixing both issues.

--- lua/neotest-gradle/hooks/find_project_directory.lua
+++ lua/neotest-gradle/hooks/find_project_directory.lua
@@ -1,3 +1,10 @@
 --- Construct function based on Neotest utility that finds root directory of
 --- a potential Gradle project based on certain indicator files.
-return require('neotest.lib').files.match_root_pattern('build.gradle', 'build.gradle.kts')
+return require('neotest.lib').files.match_root_pattern(
+  'build.gradle',
+  'build.gradle.kts',
+  'settings.gradle.kts',
+  'settings.gradle',
+  'gradlew',
+  'gradlew.bat'
+)
